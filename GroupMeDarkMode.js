8df82a86ea8f742605a80811285eac2ba4034831
Daniel Royer <2CATteam@gmail.com>
2022-02-08 16:52:31 -0600

    Adding comments and coloring notifications

--- GroupMeDarkMode.js
+++ GroupMeDarkMode.js
@@ -1,10 +1,9 @@
 function makeDark() {
 	
-    jQuery('*').each(function() {
-        let shouldFilter = false
-        if ( jQuery(this).css('background-color') == 'rgb(247, 247, 247)') jQuery(this).css('background-color', '#222');
-        else if ( jQuery(this).css('background-color') == 'rgb(255, 255, 255)') jQuery(this).css('background-color', '#000000');
-        else if ( jQuery(this).css('background-color') == 'rgb(229, 241, 246)') jQuery(this).css('background-color', '#112');
+    jQuery('*').each(function() { //For literally everything, check colors and set backgrounds
+        if ( jQuery(this).css('background-color') == 'rgb(247, 247, 247)') jQuery(this).css('background-color', '#222'); //If this uses the default light background, use a darker background
+        else if ( jQuery(this).css('background-color') == 'rgb(255, 255, 255)') jQuery(this).css('background-color', '#000000'); //If this uses a white background, use a black background
+        else if ( jQuery(this).css('background-color') == 'rgb(229, 241, 246)') jQuery(this).css('background-color', '#112'); //All of the following set the background color for messages you send for different themes
         else if ( jQuery(this).css('background-color') == 'rgb(246, 246, 229)') jQuery(this).css('background-color', '#221');
         else if ( jQuery(this).css('background-color') == 'rgb(241, 229, 246)') jQuery(this).css('background-color', '#212');
         else if ( jQuery(this).css('background-color') == 'rgb(229, 246, 229)') jQuery(this).css('background-color', '#121');
@@ -12,29 +11,38 @@ function makeDark() {
         else if ( jQuery(this).css('background-color') == 'rgb(246, 229, 229)') jQuery(this).css('background-color', '#211');
         
     })
-    jQuery('span').each(function() {
-        if ( jQuery(this).css('color') == 'rgb(59, 59, 59)') jQuery(this).css('color', '#FFF');
-        if ( jQuery(this).css('color') == 'rgb(43, 43, 48)') jQuery(this).css('color', '#CCC');
-        if ( jQuery(this).css('color') == 'rgb(98, 111, 130)') jQuery(this).css('color', '#AAB')
-        if ( jQuery(this).css('color') == 'rgb(0, 0, 0)') jQuery(this).css('color', '#FFF')
+    jQuery('span').each(function() { //For just <span> tags (which is most of the text
+        if ( jQuery(this).css('color') == 'rgb(59, 59, 59)') jQuery(this).css('color', '#FFF'); // Set grey text to white
+		  if ( jQuery(this).css('color') == 'rgb(62, 62, 62)') jQuery(this).css('color', '#FFF'); // Set notification text to white
+        if ( jQuery(this).css('color') == 'rgb(43, 43, 48)') jQuery(this).css('color', '#CCC'); //Set darker text to off-white
+        if ( jQuery(this).css('color') == 'rgb(98, 111, 130)') jQuery(this).css('color', '#AAB') //Set greyer text (typically the chat preview text) to slightly blue white
+        if ( jQuery(this).css('color') == 'rgb(0, 0, 0)') jQuery(this).css('color', '#FFF') //Set totally black text to white
     })
-    jQuery('p').css('color', "#FFF")
-    jQuery('.chat-messages,.message,.nickname').css('color', "#BBB !important")
-    jQuery(':header').css('color', "#FFF")
+	 //Set notification title color to soft white
+	 jQuery('div').each(function() {
+		 if (jQuery(this).css('color') == "rgb(62, 62, 62)") {
+			 jQuery(this).css('color', "#AAA")
+		 }
+	 })
+	 jQuery('strong').css('color', "#FFF") //All strongs are black, so make them white
+    jQuery('p').css('color', "#FFF") //All paragraphs are black, so make them white
+    jQuery('.chat-messages,.message,.nickname').css('color', "#BBB !important") //Make all of thes elements a soft white
+    jQuery(':header').css('color', "#FFF") //Set all heard tags to white
     jQuery(".chat-header").filter(function() {
-        return (jQuery(this).css('background-image').includes("assets/banners"))
-    }).find(":header").css('color', "#000")
-    jQuery('.nickname,.timestamp-divider').css('color', "#AAA")
-    jQuery('.message.system').css('background-color', "#333")
-    jQuery('.chats .list-item').unbind('mouseenter mouseleave')
-    jQuery('.chats .list-item').mouseenter(function() {
-        jQuery(this).css("background-color", "#000")
+        return (jQuery(this).css('background-image').includes("assets/banners")) //Find any tags for themed groups
+    }).find(":header").css('color', "#000") //Revert the above changes
+    jQuery('.nickname,.timestamp-divider').css('color', "#AAA") //Make all these elements a soft white
+    jQuery('.message.system').css('background-color', "#333") //Darken system messages
+    jQuery('.chats .list-item').unbind('mouseenter mouseleave') //Reset mouse events
+    jQuery('.chats .list-item').mouseenter(function() { //Set mouse events for chat list
+        jQuery(this).css("background-color", "#000") //Make pure black for hovered text
     }).mouseleave(function() {
-        jQuery(this).css("background-color", "#222")
+        jQuery(this).css("background-color", "#222") //Make softer black for un-hovered text
     })
-	 jQuery(".message-composer").css("color", "#FFF")
+	 jQuery(".message-composer").css("color", "#FFF") //Make the message composition box use white text
 }
 
+//Make an observer to have the above function run every time the DOM updates
 let DOMObserver = new MutationObserver(makeDark)
 
 // Select the node that will be observed for mutations
@@ -43,4 +51,5 @@ const targetNode = document.getElementById('');
 // Options for the observer (which mutations to observe)
 const config = { attributes: false, childList: true, subtree: true };
 
+//Observe the whole body
 DOMObserver.observe(document.body, config)
